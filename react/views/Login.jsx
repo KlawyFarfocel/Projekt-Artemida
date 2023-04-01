8268be10906bdb8e6808778e07ad293a7d05fe69
KlawyFarfocel <smierciak.maciej@gmail.com>
2023-04-01 18:49:12 +0200

    Logowanie

--- react/views/Login.jsx
+++ react/views/Login.jsx
@@ -1,23 +1,47 @@
 import { Link } from "react-router-dom"
+import { useRef, useState } from "react";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../context/ContextProvider";
 export default function Login(){
-    const onSumbit= (e) =>{
-        e.preventDefault()
+    const legitymacjaRef=useRef();
+    const passRef=useRef();
+    const [errors,setErrors] = useState(null);
+    const {setUser,setToken}=useStateContext();
+    
+    const onSubmit= (e) =>{
+        e.preventDefault();
+        const payload={
+            legitymacja: legitymacjaRef.current.value,
+            password: passRef.current.value,
+        }
+        axiosClient.post('/login',payload)
+        .then(({data})=>{
+            setUser(data.user)
+            setToken(data.token)
+        })
+        .catch(err=>{
+            const response=err.response;
+            if(response && response.status==422){
+                setErrors(response.data.errors);
+            }
+        })
     }
     return(
-        <div className="login-signup-form animated fadeInDown">      
-            <div className="form">
-                <form onSubmit={onSumbit}>
+                <form onSubmit={onSubmit}>
                     <h1 className="title">Log into your account</h1>
-                    <input type="email" placeholder="Email" />
-                    <input type="passsword" placeholder="Password" />
+                    {errors && 
+                        <div className="alert">
+                            {Object.keys(errors).map(key=>(
+                                <p>{errors[key][0]}</p>
+                            ))}
+                        </div>
+                    }
+                    <input type="text" ref={legitymacjaRef} placeholder="Numer legitymacji" />
+                    <input type="passsword" ref={passRef} placeholder="Hasło" />
                     <button className="btn btn-block">Login</button>
                     <p className="message">
                         Not Registered? <Link to="/Signup">Create an account</Link>
                     </p>
                 </form>
-            </div>
-             <p className="message" style={{"margin":"50px"},{"width":"350px"}}>Wchodzisz w \react\context\ContextProvider.jsx i w linii 14 wpisujesz cokolwiek w te nawiasy okrągłe - wtedy cię loguje</p>   
-        </div>
-       
     )
 }
