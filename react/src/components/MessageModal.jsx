e491d9808830051c269ce63b8c9184692ea79862
KlawyFarfocel <smierciak.maciej@gmail.com>
2023-04-19 14:42:04 +0200

    aaa

--- react/src/components/MessageModal.jsx
+++ react/src/components/MessageModal.jsx
@@ -13,7 +13,7 @@ return (
         <Modal.Header bsPrefix="modal-header text-center" closeButton>
           <Modal.Title bsPrefix="modal-title w-100">Ogłoszenie</Modal.Title>
         </Modal.Header>
-        <Modal.Body bsPrefix='modal-body mb-0 pb-0'>
+        <Modal.Body bsPrefix='modal-body mb-3 pb-0'>
           <span>Nadawca: </span> <span>{(props.content)?(props.content.Nadawca):("")}</span>
           <hr className="text-divider" />
           <span>Data wysłania: </span> <span>{(props.content)?(props.content['Data wysłania']):("")}</span>
@@ -23,17 +23,6 @@ return (
           <span>Treść: </span>
           <p>{(props.content)?(props.content.Treść):("")}</p>
         </Modal.Body>
-        <Modal.Footer bsPrefix="modal-footer w-100 mx-auto">
-            <div className='w-50 mx-auto d-flex'>
-              <Button bsPrefix="btn btn-success" variant="secondary" onClick={()=>props.setModalShow(false)}>
-              Odpisz
-            </Button>
-            <div className='w-25 mx-auto'></div>
-            <Button  bsPrefix="btn btn-danger" variant="primary" onClick={()=>props.setModalShow(false)}>
-              Zamknij
-            </Button>
-            </div>
-        </Modal.Footer>
       </Modal>
     </>
   );
